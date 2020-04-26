6cecf64429e5488ac0f03c80b8951089cd4b2aba
EngFelix <felix.enghofer@tum.de>
2020-04-26 16:17:54 -0700

    Commit on Victoria-PC
    Former-commit-id: 1191b40cc6a0b1c5f2466e7f0756dfe8e4f239ed
    Former-commit-id: 7b00caf6f17a5badba6720e92d2227b9a7c6692f [formerly efcde87eb8e14743ddecc52bf35bdf9d90bf9063]
    Former-commit-id: f387900ebc025e24ebb79d6efbb149d998786dda

--- Plugins/ChangeGitOrigin.cmd
+++ Plugins/ChangeGitOrigin.cmd
@@ -1,30 +1,27 @@
 @echo off
 FOR %%x in (
-	MegascansPlugin
-	nDisplayExtensions
-	rtAuralization
+	OptitrackNatNet
 ) do (
 	cd %%x
 	git remote rm origin
-	git remote add origin Y:/students/Enghofer_Felix/Git/%%x.git
+	git remote add origin https://github.com/EngFelix/%%x.git
 	git config master.remote origin
 	git config master.merge refs/heads/master
 	git remote -v
-	git push origin master
+	git pull origin master
 	cd ..
 )
 cd ../..
 FOR %%x in (
-	rtSOFETemplate
-	AIPDemo
+
 ) do (
 	cd %%x
 	git remote rm origin
-	git remote add origin Y:/students/Enghofer_Felix/Git/%%x.git
+	git remote add origin https://github.com/EngFelix/%%x.git
 	git config master.remote origin
 	git config master.merge refs/heads/master
 	git remote -v
-	git push origin master
+	git pull origin master
 	cd ..
 )
 pause
